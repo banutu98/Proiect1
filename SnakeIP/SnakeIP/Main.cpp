067b9c375d6a1ecfa823954006e27be862bbdf78
banutu98 <serbanstol@yahoo.com>
2017-01-03 18:13:05 +0200

    Snake_speed + star_implementation

--- SnakeIP/SnakeIP/Main.cpp
+++ SnakeIP/SnakeIP/Main.cpp
@@ -12,7 +12,12 @@ struct food
 {
 	int x=8, y=10;
 }f;
-int directie = 2, lungime = 4, lungime_init = lungime;
+struct stea
+{
+	int x=12, y=4;
+}spec;
+int directie = 2, lungime = 4, lungime_init = lungime, nr_mancate = 0, OK = 1, nr_mutari = 0;
+float delay = 0.2;
 void Rotire()
 {
 	for (int index = lungime; index>0; index--)
@@ -28,17 +33,30 @@ void Rotire()
 		s[0].x += 1;
 	if (directie == 3)
 		s[0].y -= 1;
-	if (s[0].x == f.x&&s[0].y == f.y)
+	if (s[0].x == f.x && s[0].y == f.y)
 	{
 		lungime++;
 		f.x = rand() % 800/16;
 		f.y = rand() % 640/16;
+		if (nr_mancate % 2 == 0) 
+		{
+			delay -= 0.01; 
+			nr_mutari = 0;
+			OK = 1;
+		}
+	}
+	if (s[0].x == spec.x && s[0].y == spec.y)
+	{
+		lungime++;
+		spec.x = rand() % 800 / 16;
+		spec.y = rand() % 640 / 16;
+		if (nr_mancate % 2 == 0) delay -= 0.004;
 	}
 }
 int main()
 {
 	srand(0);
-	sf::RenderWindow window(sf::VideoMode(800, 640), "Snake v2.0");
+	sf::RenderWindow window(sf::VideoMode(800, 640), "Snake v2.1");
 	window.setFramerateLimit(60);
 	sf::Texture t1, t2, t3;
 	t1.loadFromFile("green.png");
@@ -48,7 +66,8 @@ int main()
 	sf::Sprite mancare(t2);
 	sf::Sprite stea(t3);
 	sf::Clock clock;
-	float timer = 0, delay = 0.1;
+	float timer = 0;
+	int aparut = 0;
 
 	while (window.isOpen())
 	{
@@ -76,24 +95,32 @@ int main()
 		{
 			timer = 0;
 			Rotire();
+			if (aparut == 1 && OK==1)
+				nr_mutari++;
 		}
 		window.clear();
-
+		
+		if ((lungime_init - lungime) % 5 == 0 && lungime != lungime_init)
+		{
+			aparut = 1;
+			if (OK == 1 && nr_mutari<=30)
+			{
+				stea.setPosition(spec.x * 16, spec.y * 16);
+				window.draw(stea);
+			}
+			else 
+			{
+				window.clear();
+				OK = 0;
+			}
+		}
 		for (int index = 0; index < lungime; index++)
 		{
 			sarpe.setPosition(s[index].x * 16, s[index].y * 16);
 			window.draw(sarpe);
 		}
-		if ((lungime_init - lungime) % 5 == 0 && lungime != lungime_init)
-		{
-			stea.setPosition(f.x * 16, f.y * 16);
-			window.draw(stea);
-		}
-		else
-		{
 			mancare.setPosition(f.x * 16, f.y * 16);
 			window.draw(mancare);
-		}
 		window.display();
 	}
 
